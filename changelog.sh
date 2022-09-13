40794611b922e91542cec162a873c96ee38d7702
Toomas Vendelin <toomas.vendelin@ionos.com>
2022-09-13 15:02:41 +0200

    Add changelog.sh
    Signed-off-by: Toomas Vendelin <toomas.vendelin@ionos.com>

---
+++ changelog.sh
@@ -0,0 +1,9 @@
+git tag -l|while read T
+ do 
+    git tag -l --format='Myproject (%(tag)) all; urgency=medium' "$T"
+    echo ''
+    git tag -l --format="* %(subject)" "$T"|sed -e 's/^/  /'
+    git tag -l --format="%(body)" "$T"|sed -e 's/^/  /'
+    git tag -l --format="%(taggername) %(taggeremail)  %(taggerdate)" "$T"
+    echo ''
+ done
