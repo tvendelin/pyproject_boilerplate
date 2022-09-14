451ead4db81fa7422cb5422b2556aac64e234c03
Toomas Vendelin <toomas.vendelin@ionos.com>
2022-09-14 07:31:58 +0200

    Sort tags by version desc
    Signed-off-by: Toomas Vendelin <toomas.vendelin@ionos.com>

--- changelog.sh
+++ changelog.sh
@@ -1,6 +1,6 @@
-git tag -l|while read T
+git tag -l --sort=-version:refname | while read T
  do 
-    git tag -l --format='Myproject (%(tag)) all; urgency=medium' "$T"
+    git tag -l  --format='Myproject (%(tag)) all; urgency=medium' "$T"
     echo ''
     git tag -l --format="* %(subject)" "$T"|sed -e 's/^/  /'
     git tag -l --format="%(body)" "$T"|sed -e 's/^/  /'
